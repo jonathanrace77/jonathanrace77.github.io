5d8b9620c45f60f50cb29f0a4acb512ec49d7303
Jonathan 772 <jonathanrace@me.com>
2020-01-23 11:08:39 +0000

    1.0.1

--- jquery.js
+++ jquery.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-    $("h1").hide().fadeIn(3000);
+    $("h1").hide().fadeIn(2500);
     $("p").hide().fadeIn(4000);
     });
+
+$(document).ready(function() {
+    $("#navbar").animate({ top: '0px' }, 800);
+    });
+
