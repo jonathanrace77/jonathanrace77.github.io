00f3e77fdb7f80b277ea175041332398ac54c482
Jonathan 772 <jonathanrace@me.com>
2020-02-19 12:17:39 +0000

    1.1.5
    portfolio section updated

--- jquery.js
+++ jquery.js
@@ -60,3 +60,16 @@ $(document).ready(function() {
     }
   );
 });
+
+//Bouncing arrow icon
+$('img').hover( 
+  function() { $(this).data('bounce', true); bounce($(this));}, 
+  function() { $(this).data('bounce', false);
+});
+
+function bounce($elem) {
+  $elem.effect('bounce', { times: 1, distance: 10 }, 500, function() {
+      if ($(this).data('bounce')) bounce($elem);
+      else $elem.stop();
+  });
+}
