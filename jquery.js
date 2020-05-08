1a59626e38bfc38e62f8d35ced43535904449db7
Jonathan 772 <jonathanrace@me.com>
2020-05-08 08:31:54 +0100

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     })
     .slick({
       slidesToShow: 3,
-      slidesToScroll: 9,
+      slidesToScroll: 3,
       dots: false,
       focusOnSelect: false,
       infinite: true,
@@ -43,21 +43,21 @@ $(document).ready(function () {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 9,
+            slidesToScroll: 3,
           },
         },
         {
           breakpoint: 640,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 9,
+            slidesToScroll: 3,
           },
         },
         {
           breakpoint: 420,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 9,
+            slidesToScroll: 3,
           },
         },
       ],
